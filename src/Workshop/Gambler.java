857d16e8c800f94675a2e88d6e781ecda374064d
dhanrajpandagale <dhanrajpandagale@gmail.com>
2022-06-28 09:29:57 -0400

    Gambling_UC1

---
+++ src/Workshop/Gambler.java
@@ -0,0 +1,15 @@
+package Workshop;
+
+public class Gambler {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Gambler game");
+
+		int EveryDayStack = 100;
+		int BetForGame = 1;
+
+		System.out.println("Every Day Stack is " + EveryDayStack);
+		System.out.println("Bet Price: " + BetForGame);
+
+}
+}
